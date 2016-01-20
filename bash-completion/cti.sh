1c40408f3a90085e9fd0c9a4c72ae6a32aeb399f
root <github@rochefort.de>
2016-01-20 23:46:30 +0100

    cti: bash completion script

---
+++ bash-completion/cti.sh
@@ -0,0 +1,19 @@
+complete -f -W \
+    "add \
+    check \
+    check-all \
+    edit \
+    remove \
+    restore \
+    sync \
+    sync-all \
+    untrack \
+    --abort-on-conflict \
+    --backup-on-conflict \
+    --debug \
+    --help \
+    --ignore-on-conflict \
+    --restore-on-conflict \
+    --suppress-synced \
+    --untrack-on-conflict \
+    " cti
