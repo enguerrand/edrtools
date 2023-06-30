7741289a8dcc5fc4e0c605a440a3d9fe9178e2e5
edr <github@rochefort.de>
2023-06-30 21:45:45 +0200

    systemd-check-daily-cronjob.sh: Fix error in script when no units are failed

--- systemd-check-daily-cronjob.sh
+++ systemd-check-daily-cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 failed_unit_count=$(systemctl --failed --quiet | grep "units listed" | grep -o "^[0-9]* ")
-[ ${failed_unit_count} -eq 0 ] && exit 0
+[ ${failed_unit_count:-0} -eq 0 ] && exit 0
 systemctl --failed >&2
