4e27c3c22f1056471067c728fb43b5250341c1aa
edr <github@rochefort.de>
2023-09-01 21:33:33 +0200

    systemd-check-daily-cronjob.sh: remove --quiet flag

--- systemd-check-daily-cronjob.sh
+++ systemd-check-daily-cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-failed_unit_count=$(systemctl --failed --quiet | grep "units listed" | grep -o "^[0-9]* ")
+failed_unit_count=$(systemctl --failed  | grep "units listed" | grep -o "^[0-9]* ")
 [ ${failed_unit_count:-0} -eq 0 ] && exit 0
 systemctl --failed >&2
